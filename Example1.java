776c135fa21d469eaaee576ef6f7719e9f4b31fb
Tejasvi Chaudhary <86316105+TejasviChaudhary@users.noreply.github.com>
2022-08-05 22:29:25 +0530

    Create Example1.java

---
+++ Example1.java
@@ -0,0 +1,26 @@
+public class Example1 {
+  public static void main(String[] args) {
+    int a = 0;
+    if (a >= 0) {
+      System.out.println(">= 0");
+      if (a != 0) {
+        System.out.println("!= 0");
+      }
+    } else {
+      System.out.println("< 0");
+    }
+  }
+}
+/*public class Example2 {
+  public static void main(String[] args) {
+    int a = 0;
+    if (a >= 0) {
+      System.out.println(">= 0");
+    } else {
+      System.out.println("< 0");
+    }
+    if (a != 0) {
+      System.out.println("!= 0");
+    }
+  }
+}*/
